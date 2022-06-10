89b43059beb26c58f5c608ba45d9ff1953f263e9
youngmin <min050410@gmail.com>
2022-06-11 00:00:26 +0900

    add: todo entity, typeorm 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoController } from './todo/todo.controller';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mariadb',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [__dirname + '/**/entities/*.entity.{js,ts}']
+  }),
+    TodoModule
+  ],
+  controllers: [AppController, TodoController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/todo/entity/todo.entity.ts
@@ -0,0 +1,6 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity('todo')
+export class TodoEntity {
+   
+}
